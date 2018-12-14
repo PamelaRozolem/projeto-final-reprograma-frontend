aee915b1a1eefdaabdcd34fab78327c51829684d
pam <pamelarozolem@gmail.com>
2018-12-14 15:05:30 -0200

    ajustes finais

--- src/paginas/HomePagina/HomePagina.js
+++ src/paginas/HomePagina/HomePagina.js
@@ -7,10 +7,8 @@ function HomePagina() {
   return (
     <main className="home-pagina banner">
       <div className="card">
-        <h1>Especializados em gerencia.</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-          Morbi sed est faucibus, imperdiet nibh non, vulputate orci. 
-        </p>
+        <h1>Control Your Future Wealth.</h1>
+        <p>All-in-one wallet to secure, manage and exchange blockchain assets</p>
         <Link className="link-registre" to="/registre"><span class="fas fa-jedi"></span>  Registre-se</Link> 
         <Link className="link-acesse" to="/login"> <span class="fas fa-user"></span> Acesse</Link>
       </div>

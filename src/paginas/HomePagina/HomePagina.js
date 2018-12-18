b98272ad0bcadaa3c179e2372dc2c3d63c83d553
pam <pamelarozolem@gmail.com>
2018-12-18 11:38:53 -0200

    alterando css cardUser banner

--- src/paginas/HomePagina/HomePagina.js
+++ src/paginas/HomePagina/HomePagina.js
@@ -8,7 +8,7 @@ function HomePagina() {
     <main className="home-pagina banner">
       <div className="card">
         <h1>Controle sua futura riqueza.</h1>
-        <p>Carteira All-in-one para proteger, gerenciar e trocar ativos blockchain</p>
+        <p>Carteira All-in-one para proteger, gerenciar e trocar ativos blockchain.</p>
         <Link className="link-registre" to="/registre"><span class="fas fa-jedi"></span>  Registre-se</Link> 
         <Link className="link-acesse" to="/login"> <span class="fas fa-user"></span> Acesse</Link>
       </div>
